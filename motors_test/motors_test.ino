754391e5eb8db65cf09d4e292ec8a7532de32b61
MW <None>
2020-03-19 12:03:55 +0100

    Motor test

---
+++ motors_test/motors_test.ino
@@ -0,0 +1,54 @@
+//Define which pins to use for Motors
+//First Motor
+int on_first = ;
+int FMotor_Forward = ;
+int FMotor_Backward = ;
+
+//Second Motor
+int on_second = ;
+int SMotor_Forward = ;
+int SMotor_Backward = ;
+
+void setup() {
+//Set the baud rate for serial data transmission  
+Serial.begin(9600)  
+//Pins configuration
+pinMode(on_first, OUTPUT);
+pinMode(FMotor_Forward, OUTPUT);
+pinMode(FMotor_Backward, OUTPUT);
+
+pinMode(on_second,OUTPUT);
+pinMode(SMotor_Forward,OUTPUT);
+pinMode(SMotor_Backward,OUTPUT);
+
+}
+
+void loop() {
+//Starting Motors
+digitalWrite(on_first, HIGH);
+digitalWrite(on_second, HIGH);
+delay(1000);
+
+//Move backwards
+digitalWrite(FMotor_Backward, HIGH);
+digitalWrite(FMotor_Forward, LOW);
+
+digitalWrite(SMotor_Backward, HIGH);
+digitalWrite(SMotor_Forward, LOW);
+
+delay(1000);
+
+//Move forward
+digitalWrite(FMotor_Backward, LOW);
+digitalWrite(FMotor_Forward, HIGH);
+
+digitalWrite(SMotor_Backward, LOW);
+digitalWrite(SMotor_Forward, HIGH);
+
+delay(1000);
+//Stop motors
+digitalWrite(on_first, LOW);
+digitalWrite(on_second, LOW);
+Serial.println("Stopped motors");
+delay(5000);
+}
