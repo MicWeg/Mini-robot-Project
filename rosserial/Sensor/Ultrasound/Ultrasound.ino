365233fb8182827e48e6dc111bbdde0d147e4834
MW <None>
2020-04-29 14:42:27 +0200

    rosserial

---
+++ rosserial/Sensor/Ultrasound/Ultrasound.ino
@@ -0,0 +1,155 @@
+#include <ros.h>
+#include <ros/time.h>
+#include <sensor_msgs/Range.h>
+#include <std_msgs/String.h>
+
+//Define which pins to use for sensor
+#define Trigger 2
+#define Echo 3
+
+//Define which pins to use for Motors
+//First Motor
+#define on_first 6
+#define FMotor_Forward 7
+#define FMotor_Backward 8
+
+//Second Motor
+#define on_second 9
+#define SMotor_Forward 10
+#define SMotor_Backward 11
+
+//Create handler
+ros::NodeHandle  nh;
+
+sensor_msgs::Range range_msg;
+ros::Publisher pub_range( "/ultrasound", &range_msg);
+
+
+char frameid[] = "/ultrasound";
+
+//Motors pins configuration
+void motor_setup() {
+  pinMode(on_first, OUTPUT);
+  pinMode(FMotor_Forward, OUTPUT);
+  pinMode(FMotor_Backward, OUTPUT);
+  
+  pinMode(on_second,OUTPUT);
+  pinMode(SMotor_Forward,OUTPUT);
+  pinMode(SMotor_Backward,OUTPUT);
+}
+
+//Sensor pins configuration
+void sensor_setup() {
+  pinMode(Trigger, OUTPUT);
+  pinMode(Echo, INPUT);
+}
+
+//Declare detection range in cm
+const int max_range = 200;
+const int min_range = 2;
+
+//Measue 50ms interval for sensor
+long range_time;
+
+
+float getRange_Ultrasound(){
+  int distance;
+
+    //Set the Trigger to LOW for 2 microseconds
+    digitalWrite(Trigger, LOW);
+    delayMicroseconds(2);
+  
+    //Set the Trigger to HIGH for 10 microseconds to emit 40kHz sound pulse
+    digitalWrite(Trigger, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(Trigger, LOW);
+  
+    //Read the lenght of the pulse in microseconds and add to variable 
+    distance = pulseIn(Echo, HIGH) ;
+
+  //Calculate  distance in cm (4*58.2)
+  return distance/58.2;
+}
+
+void move(int distance){
+
+  //Move backward if distance between robot and obstacle is equal or less than 2 centimeters 
+  if(distance <= 2 && distance >=0){
+    //Enable in case the robot was turned on in front of an obstacle
+    digitalWrite(on_first,HIGH);
+    digitalWrite(on_second,HIGH);
+
+    digitalWrite(FMotor_Forward,LOW);
+    digitalWrite(FMotor_Backward,HIGH);
+
+    digitalWrite(SMotor_Forward,LOW);
+    digitalWrite(SMotor_Backward,HIGH);
+
+    delay(500);
+
+    //Move right or left depending on pseudo-random number generator
+    if(random(2) == 1){
+      
+      digitalWrite(FMotor_Forward,LOW);
+      digitalWrite(FMotor_Backward,HIGH);
+  
+      digitalWrite(SMotor_Forward,HIGH);
+      digitalWrite(SMotor_Backward,LOW);
+    }
+    else{
+      
+      digitalWrite(FMotor_Forward,HIGH);
+      digitalWrite(FMotor_Backward,LOW);
+  
+      digitalWrite(SMotor_Forward,LOW);
+      digitalWrite(SMotor_Backward,HIGH);
+
+    }
+    delay(1000);
+  }
+  //Move forward
+  digitalWrite(on_first,HIGH);
+  digitalWrite(on_second,HIGH);
+
+  digitalWrite(FMotor_Forward,HIGH);
+  digitalWrite(FMotor_Backward,LOW);
+
+  digitalWrite(SMotor_Forward,HIGH);
+  digitalWrite(SMotor_Backward,LOW);
+
+  delay(50);
+}
+
+void ros_sensor(){
+  
+  if ( (millis()-range_time) > 50 ){
+  
+      range_msg.range = getRange_Ultrasound();
+      range_msg.header.stamp = nh.now();
+      pub_range.publish(&range_msg);
+      range_time =  millis() + 50;
+    }
+}
+
+
+void setup(){
+  nh.initNode();
+  nh.advertise(pub_range);
+   
+  range_msg.radiation_type = sensor_msgs::Range::ULTRASOUND;
+  range_msg.header.frame_id =  frameid;
+  range_msg.field_of_view = 0.26;  
+  range_msg.min_range = min_range;
+  range_msg.max_range = max_range;
+  
+  sensor_setup();
+  motor_setup();
+}
+
+void loop()
+{
+  randomSeed(analogRead(0));
+  ros_sensor();
+  move(getRange_Ultrasound());
+  nh.spinOnce();
+}
